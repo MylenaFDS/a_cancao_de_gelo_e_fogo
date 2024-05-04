abd3d9e0b2c7cac81c6bf6c1718eee7517268fad
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-05-03 21:20:19 -0300

    seção elenco

--- src/main.js
+++ src/main.js
@@ -6,9 +6,19 @@ let counter = 0;
 nextBtn.addEventListener('click', () => {
     counter++;
     carousel.style.transform = `translateX(-${counter * 100}%)`;
+    prevBtn.classList.remove('hidden');
 });
 
 prevBtn.addEventListener('click', () => {
-    counter--;
-    carousel.style.transform = `translateX(-${counter * 100}%)`;
+    if (counter > 0) { // Verifica se o contador é maior que zero para evitar valores negativos
+        counter--;
+        carousel.style.transform = `translateX(-${counter * 100}%)`;
+    }
+    if (counter === 0) {
+        prevBtn.classList.add('hidden');
+    } else {
+        nextBtn.classList.remove('hidden');
+    }
 });
+
+
