644aa9b7295047c6b8b51a0b91758ea0359361c0
Mylena Ferreira de Souza <mylenafds@gmail.com>
2024-05-12 21:14:22 -0300

    ..

--- src/main.js
+++ src/main.js
@@ -80,3 +80,34 @@ document.querySelector('[data-tab-button="temporada_1"]').addEventListener('clic
         temporada1ButtonContainer.style.display = 'none';
     }
 });
+document.addEventListener('DOMContentLoaded',function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    
+
+    for (let i=0;i<buttons.length;i++){
+        buttons[i].addEventListener('click',function(botao){
+            const abaAlvo= botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+})
+
+function removeBotaoAtivo(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i=0;i<buttons.length;i++){
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
+function escondeTodasAbas(){
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for(let i=0;i<tabsContainer.length;i++){
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
